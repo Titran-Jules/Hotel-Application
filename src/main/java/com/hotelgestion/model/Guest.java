4faaf8f0ed8d58d20bcc7e33c70c181c4228b1f0
Adri <mandaramilison19@gmail.com>
2026-06-18 20:41:39 +0300

    fix: correct Reservation constructor call in Guest.book()

---
+++ src/main/java/com/hotelgestion/model/Guest.java
@@ -0,0 +1,20 @@
+package com.hotelgestion.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+
+public class Guest {
+
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Reservation book(Room r, LocalDate startDate, LocalDate endDate) {
+        return new Reservation(0, this, r, startDate, endDate, ReservationStatus.PENDING, 0.0);
+    }
+}
